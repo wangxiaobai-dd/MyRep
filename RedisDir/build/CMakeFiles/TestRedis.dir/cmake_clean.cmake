d8e601228786bd5914cda0f260bb5cb0abdadde3
wangxiaobai-dd <leoeee@icloud.com>
2020-03-29 23:27:20 +0800

    sync redis

---
+++ RedisDir/build/CMakeFiles/TestRedis.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestRedis.dir/TestRedis.cpp.o"
+  "TestRedis.pdb"
+  "TestRedis"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestRedis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
