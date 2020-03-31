2ed90bc5d3783bc56ef4a657f2a48e14a0fdf87a
wangxiaobai-dd <leoeee@icloud.com>
2020-03-31 17:15:37 +0800

    cmake test:w

---
+++ TestCMakeDir/build/CMakeFiles/TestCMake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestCMake.dir/TestCMake.cpp.o"
+  "TestCMake.pdb"
+  "TestCMake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestCMake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ TestCMakeDir/build/CMakeFiles/demo9_lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo9_lib.dir/TestCMake.cpp.o"
+  "libdemo9_lib.pdb"
+  "libdemo9_lib.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo9_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
