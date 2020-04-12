15cb6ed4b699f767f0fd8d32699e1a6c07509706
wangxiaobai-dd <leoeee@icloud.com>
2020-04-12 21:38:53 +0800

    protobuf

--- ToolDir/build/CMakeFiles/Makefile.cmake
+++ ToolDir/build/CMakeFiles/Makefile.cmake
@@ -11,6 +11,7 @@ set(CMAKE_MAKEFILE_DEPENDS
   "CMakeFiles/3.10.2/CMakeCCompiler.cmake"
   "CMakeFiles/3.10.2/CMakeCXXCompiler.cmake"
   "CMakeFiles/3.10.2/CMakeSystem.cmake"
+  "../proto/CMakeLists.txt"
   "/usr/share/cmake-3.10/Modules/CMakeCInformation.cmake"
   "/usr/share/cmake-3.10/Modules/CMakeCXXInformation.cmake"
   "/usr/share/cmake-3.10/Modules/CMakeCommonLanguageInclude.cmake"
@@ -41,9 +42,11 @@ set(CMAKE_MAKEFILE_OUTPUTS
 # Byproducts of CMake generate step:
 set(CMAKE_MAKEFILE_PRODUCTS
   "CMakeFiles/CMakeDirectoryInformation.cmake"
+  "proto/CMakeFiles/CMakeDirectoryInformation.cmake"
   )
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/EventLoop.dir/DependInfo.cmake"
+  "proto/CMakeFiles/testProto.dir/DependInfo.cmake"
   )
---
+++ ToolDir/build/proto/CMakeFiles/testProto.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../../proto/message.pb.h"
+  "../../proto/message.pb.cc"
+  "CMakeFiles/testProto.dir/message.pb.cc.o"
+  "../libtestProto.pdb"
+  "../libtestProto.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testProto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
