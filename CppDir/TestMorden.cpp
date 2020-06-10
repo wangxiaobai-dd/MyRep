9eb562e33e905884168b571259a391dde52beb41
wangxiaobai-dd <leoeee@icloud.com>
2020-06-10 20:07:58 +0800

    SFINAE constexpr

--- CppDir/TestMorden.cpp
+++ CppDir/TestMorden.cpp
@@ -1,8 +1,9 @@
-#include <type_traits> 
+//#include <type_traits> 
 #include <iostream>
 #include <map>
 #include <set>
 #include <unordered_map>
+#include <vector>
 
 template <class T, T v>
 struct integral_constant {
@@ -60,6 +61,7 @@ struct has_reserve
         static const bool value = sizeof(reserve<T>(nullptr) == sizeof(good));	
 };
 
+// enable_if
 template <typename Container> 
 	typename std::enable_if_t<std::is_same<Container, std::map<typename Container::key_type, typename Container::mapped_type>>::value>
 test(Container& cont)
@@ -71,7 +73,7 @@ template <typename Container>
 	std::enable_if_t<std::is_same<Container, std::set<typename Container::value_type> >::value>
 test(Container& cont)
 {
-	//cont.insert(1);
+	cont.insert(1);
 }
 
 /*
@@ -100,6 +102,7 @@ void FC(Container& fc)
 
 }
 
+// decltype
 template <typename Container>
 auto append(Container& cont)->decltype(std::declval<Container&>().insert(typename Container::value_type(typename Container::key_type(), typename Container::mapped_type())), void())
 {
@@ -108,9 +111,86 @@ auto append(Container& cont)->decltype(std::declval<Container&>().insert(typenam
 }
 
 template <typename Container>
-auto append(Container& cont)->decltype(std::declval<Container&>().insert(typename Container::key_type()), void())
+auto append(Container& cont)->std::void_t<decltype(std::declval<Container&>().insert(typename Container::key_type()))>
 {
 	cont.insert(100);
+}
+
+template <typename Container>
+auto append(Container& cont)->decltype(std::declval<Container&>().push_back(typename Container::value_type()), void())
+{
+	cont.push_back(100);
+}
+
+template <typename T>
+struct ContType
+{
+	struct NO { char tmp; };
+	struct S { char tmp[2];};
+	struct V { char tmp[3];};
+	struct M { char tmp[4];};
+	/*
+	template <class U, void(U::*)(size_t)> 
+	struct SFINAE
+	{};
+	*/
+	template <class U, void (U::*)(const typename U::value_type&)> 
+	struct SFINAE
+	{};
+
+	template <class U, std::pair<typename U::iterator, bool> (U::*)(const typename U::value_type&)> 
+	struct SFINAE_S
+	{};
+
+	template <class U> 
+	static V add(SFINAE<U, &U::push_back>*);
+
+	template <class U> 
+	static S add(SFINAE_S<U, &U::insert>*);
+
+	template <class U> 
+	static NO add(...);
+
+	static constexpr int value = sizeof(add<T>(nullptr)) == sizeof(V);
+	static constexpr int value1 = sizeof(add<T>(nullptr)) == sizeof(S);
+};
+
+template <typename Container>
+void testCT(Container& c, typename Container::value_type t)
+{
+	if constexpr (ContType<Container>::value == 1)
+	{
+		std::cout << "testCT:" << 1 << std::endl;
+		c.push_back(t);
+	}
+	else if constexpr (ContType<Container>::value1 == 1)
+	{
+		std::cout << "testCT:" << 2 << std::endl;
+		c.insert(t);
+	}
+}
+
+/*
+// ÖØ¸´¶¨Òå
+template <typename Container>
+auto append(Container& cont)->std::void_t<decltype(std::declval<Container&>().push_back(typename Container::value_type()))>
+{
+	cont.push_back(100);
+}
+*/
+
+/*
+template <template <typename T, typename CONT>typename MAP, typename T, typename CONT>
+void save(MAP<T, CONT>& m)
+{
+	test(m[1]);
+}
+*/
+
+template <template <typename T, typename CONT>typename MAP, typename T, typename CONT>
+auto save(MAP<T, CONT>& m)->decltype(std::declval<MAP<T, CONT>&>().insert(typename MAP<T, CONT>::value_type(typename MAP<T, CONT>::key_type(), typename MAP<T, CONT>::mapped_type())), void())
+{
+	test(m[1]);
 }
 
 struct MM
@@ -118,26 +198,32 @@ struct MM
 
 int main()
 {
-	//std::map<int, int> mmm;
-	std::unordered_map<int, int> mmm2;
-	std::set<int> sss;
-	std::set<MM> sss2;
-	//test(mmm);
-	//test(mmm2);
-	//test(sss);
-//	test(sss2);
-	std::map<int, std::map<int, int>> mmm;
+	std::unordered_map<int, std::map<int, int>> tmpMap1;
+	save(tmpMap1);
+
+	std::map<int, std::map<int, int>> tmpMap2;
+	append(tmpMap2);
+	FC(tmpMap2);
+
 	std::map<int, std::set<int>> sss3;
-	FC(mmm);
 	FC(sss3);
 	
-	append(mmm);
-	//append(mmm2);
-//	append2(mmm);
+	std::set<int> sss;
 	append(sss);
+	test(sss);
 
-	std::cout << mmm[1][1] << std::endl;
-	std::cout << *sss.begin() << std::endl;
+	std::vector<int> vvv;
+	append(vvv);
 
+	//std::cout << sss.size() << std::endl;
+
+	std::cout << "ContType:" << ContType<std::vector<int>>::value << std::endl;
+	std::cout << "ContType:" << ContType<std::set<int>>::value1 << std::endl;
+	std::cout << "ContType:" << ContType<std::set<int>>::value << std::endl;
+//	std::cout << "ContType:" << ContType<std::map<int, int>>::value3 << std::endl;
+//
+	testCT(vvv, 1);
+	testCT(sss, 1);
+	
 	return 0;
 }
